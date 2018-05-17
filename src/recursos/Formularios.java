1c4feb0f0e089f8db99513b120b5269c551e0aea
digodk <diogodklein@gmail.com>
2018-05-16 22:07:32 -0300

    aaa

--- src/recursos/Formularios.java
+++ src/recursos/Formularios.java
@@ -1,12 +1,18 @@
 package recursos;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
 public class Formularios {
   JFrame frame = new JFrame();
-  
+
   // Base aula
   void base() {
     // JFrame
@@ -15,19 +21,41 @@ public class Formularios {
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setLocationRelativeTo(null);
     frame.setLayout(null);
-    
+
     // JLabel
     JLabel lbl = new JLabel("texto");
     lbl.setBounds(0, 0, 100, 20);
-    
+
     // JTextFields
     JTextField campo = new JTextField();
     campo.setBounds(120, 0, 100, 20);
-    
+
+    // JButton
+    JButton botao = new JButton("Calcular");
+    botao.setBounds(120, 150, 130, 20);
+    // Listener de ação com
+    botao.addActionListener(new ActionListener() {
+
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        JOptionPane.showMessageDialog(null, "Mensagem quando clicar no botão");
+
+      }
+    });
+
+    // JComboBox
+    JComboBox<String> combo = new JComboBox<>();
+    combo.setBounds(120, 150, 130, 20);
+    combo.addItem("Opção 1");
+    combo.addItem("Opção 2");
+
     // Adicionar componentes
     frame.add(lbl);
     frame.add(campo);
+    frame.add(botao);
+    frame.add(combo);
     
+
     // Exibir
     frame.setVisible(true);
   }
---
+++ src/recursos/teste.java
@@ -0,0 +1,10 @@
+package recursos;
+
+public class teste {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    // Um comentário(' \u000d System.out.println("Olá");
+  }
+
+}
